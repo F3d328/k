982102b019ef20f252ba0ab55436f8280d891025
fedei <fedei@DESKTOP-86A29DE>
2024-05-27 12:57:43 +0200

    id-4 resolved

--- WebContent/script/Registrazione.js
+++ WebContent/script/Registrazione.js
@@ -11,8 +11,8 @@ function checkNomeCognome(inputtxt) {
 
 
 function checkEmail(inputtxt) {
-	var email = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-	if(inputtxt.value.match(email)) 
+	var email = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+	if (inputtxt.value.match(email)) 
 		return true;
 	
 	return false;	
